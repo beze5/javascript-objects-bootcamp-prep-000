46fc4c6385833dae595d085ad7bcfd38983a62a6
Benjamin Ezekiel Goode <ben.e.goode@gmail.com>
2019-06-25 15:02:02 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,8 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  Object.assign({})
-  delete playlist.artistName;
+  var newPlaylist = delete playlist.artistName;
   return playlist;
 }
 
